35eeabc33fdbb3cd25a4b8fe7cfdac006d25f127
Pulkit Shrivastava <111634860+pulshr@users.noreply.github.com>
2022-10-28 06:36:47 +0530

    Create BalancedParanthesis.java

---
+++ Stacks/BalancedParanthesis.java
@@ -0,0 +1,78 @@
+/**
+
+Problem Description
+Given an expression string A, examine whether the pairs and the orders of “{“,”}”, ”(“,”)”, ”[“,”]” are correct in A.
+
+Refer to the examples for more clarity.
+
+
+
+Problem Constraints
+1 <= |A| <= 100
+
+
+
+Input Format
+The first and the only argument of input contains the string A having the parenthesis sequence.
+
+
+
+Output Format
+Return 0 if the parenthesis sequence is not balanced.
+
+Return 1 if the parenthesis sequence is balanced.
+
+
+
+Example Input
+Input 1:
+
+ A = {([])}
+Input 2:
+
+ A = (){
+Input 3:
+
+ A = ()[] 
+
+
+Example Output
+Output 1:
+
+ 1 
+Output 2:
+
+ 0 
+Output 3:
+
+ 1 
+
+
+Example Explanation
+You can clearly see that the first and third case contain valid paranthesis.
+
+In the second case, there is no closing bracket for {, thus the paranthesis sequence is invalid.
+
+**/
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        if (strs.length == 0) return new ArrayList();
+        Map<String, List> ans = new HashMap<String, List>();
+        int[] count = new int[26];
+        for (String s : strs) {
+            Arrays.fill(count, 0);
+            for (char c : s.toCharArray()) count[c - 'a']++;
+
+            StringBuilder sb = new StringBuilder("");
+            for (int i = 0; i < 26; i++) {
+                sb.append('#');
+                sb.append(count[i]);
+            }
+            String key = sb.toString();
+            if (!ans.containsKey(key)) ans.put(key, new ArrayList());
+            ans.get(key).add(s);
+        }
+        return new ArrayList(ans.values());
+    }
+}
