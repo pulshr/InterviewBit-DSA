202cc2b9b55139bc09829c1ca7143dfc4a7a6959
Pulkit Shrivastava <111634860+pulshr@users.noreply.github.com>
2022-10-29 04:43:20 +0530

    Reverse Linked List 2

---
+++ Linked-List/ReverseLinkedList2.java
@@ -0,0 +1,117 @@
+/**
+
+Problem Description
+Reverse a linked list A from position B to C.
+
+NOTE: Do it in-place and in one-pass.
+
+
+
+Problem Constraints
+1 <= |A| <= 106
+
+1 <= B <= C <= |A|
+
+
+
+Input Format
+The first argument contains a pointer to the head of the given linked list, A.
+
+The second arugment contains an integer, B.
+
+The third argument contains an integer C.
+
+
+
+Output Format
+Return a pointer to the head of the modified linked list.
+
+
+
+Example Input
+Input 1:
+
+ A = 1 -> 2 -> 3 -> 4 -> 5
+ B = 2
+ C = 4
+
+Input 2:
+
+ A = 1 -> 2 -> 3 -> 4 -> 5
+ B = 1
+ C = 5
+
+
+Example Output
+Output 1:
+
+ 1 -> 4 -> 3 -> 2 -> 5
+Output 2:
+
+ 5 -> 4 -> 3 -> 2 -> 1
+
+
+Example Explanation
+Explanation 1:
+
+ In the first example, we want to reverse the highlighted part of the given linked list : 1 -> 2 -> 3 -> 4 -> 5 
+ Thus, the output is 1 -> 4 -> 3 -> 2 -> 5 
+Explanation 2:
+
+ In the second example, we want to reverse the highlighted part of the given linked list : 1 -> 4 -> 3 -> 2 -> 5  
+ Thus, the output is 5 -> 4 -> 3 -> 2 -> 1 
+ 
+ **/
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     ListNode(int x) { val = x; next = null; }
+ * }
+ */
+public class Solution {
+    public ListNode reverseBetween(ListNode A, int B, int C) {
+        ListNode curr = A;
+        ListNode first = null;
+        ListNode from = null;
+        ListNode till = null;
+        ListNode last = null;
+        int count = 1;
+        while(curr!=null) {
+            if(count<B) {
+                first = curr;
+            }
+            if(count==B) {
+                from = curr;
+            }
+            if(count==C) {
+                till = curr;
+                last = till.next;
+            }
+            count++;
+            curr = curr.next;
+        }
+        till.next = null;
+        if(first == null) {
+            A = reverse(from);
+        } else {
+            first.next = reverse(from);
+        }
+        from.next = last;
+        return A;
+    }
+    public ListNode reverse(ListNode A) {
+        ListNode prev = null;
+        ListNode curr = A;
+        ListNode next;
+        while(curr!=null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+}
